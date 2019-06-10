798958e13cecee3157ecd5c55f3bce2c87250dbf
weiyouq <jiayouk@163.com>
2019-06-10 17:24:39 +0800

    ss

--- src/main/java/com/xuele/log/send/kafka/formatter/MessageFormatter.java
+++ src/main/java/com/xuele/log/send/kafka/formatter/MessageFormatter.java
@@ -11,6 +11,7 @@ import ch.qos.logback.classic.spi.ILoggingEvent;
  */
 public class MessageFormatter implements Formatter {
 
+	@Override
     public String format(ILoggingEvent event) {
         return event.getFormattedMessage();
     }
